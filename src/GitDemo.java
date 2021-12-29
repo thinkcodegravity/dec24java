5c47e5c170233a48631be753bd35da487e0da452
java <74448030+thinkcodegravity@users.noreply.github.com>
2021-12-28 18:59:12 -0600

    demo

---
+++ src/GitDemo.java
@@ -0,0 +1,8 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("new demo for git upload");
+	}
+
+}
--- src/Welcome.java
+++ src/Welcome.java
@@ -1,4 +1,3 @@
-package jtrainingdec;
 
 public class Welcome {
 
