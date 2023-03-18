c180640c5ad2c441b8215639b9f3eb3c02a5328c
java <74448030+thinkcodegravity@users.noreply.github.com>
2023-03-17 23:31:59 -0500

    variable

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -6,8 +6,31 @@
 // this program is designed for deposit and withdraw
 public class BankAccount {
 	// { boundary in the program
-	accountNumber; // machines need to know
+	
+	// what kind of information you want to store = DATA TYPE
+	// what would you like to call this information = VARIABLE name ( information it holds keeps changing)
+	// what is the information itself = information. VALUE
+	
+	int accountNumber=123; // machines need to know
 					// what kind of information will be stored
 					// in accountNumber
+	double balance=17.3;
+	boolean isAccountActive;
+	String customerName;
+	long routingnumber;
+	String userid;
+	int id;
+	String password;
+	int pincode;
+	// kind of information or DATA TYPE
+	// accountNumber = 
+	/*
+	numeric = byte,short,int,long
+	fractional = float,double
+	single character = char
+	yes/no = boolean
+	group of character = String
+	complex = another program
+	*/
 	
 }
