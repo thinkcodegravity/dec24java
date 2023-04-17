9ae40eb1e91ca046243b67c119f420621fb46bd3
java <74448030+thinkcodegravity@users.noreply.github.com>
2023-04-17 10:36:38 -0500

    collection

--- src/coll/CollTest.java
+++ src/coll/CollTest.java
@@ -6,59 +6,52 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.TreeSet;
+import java.util.Vector;
 
 public class CollTest {
 
+	
 	public static void main(String[] args) {
-		String[] names=new String[5];
-		names[0]="john";
-		names[1]="jane";
-		names[4]="andy";
-		for(String s:names)
-			System.out.println(s);
+		// collections dont work with primitive/simple data type
+		// Wrapper data type = Object/complex version of
+		// primitive data type
+		// int=Integer, short=Short, byte=Byte, long=Long
+		// float=Float, double=Double, boolean=Boolean
+		// char=Character
 		
-		// list is indexed and ordered and dynamic size
-		LinkedList<String> nameColl=new LinkedList<String>();
-		nameColl.add("john");
-		nameColl.add("jane");
-		nameColl.add("andy");
-		nameColl.add("mike");
-		System.out.println(nameColl);
-		System.out.println( nameColl.indexOf("andy"));
+		Vector<Float> scores=new Vector<Float>();
+		scores.add(25.3f);
+		// 3 popular type of List collection
+		// vector = BETTER READING. synchronized ( Accurate BUT slow )
+		// arraylist = BEST READING. not synchronized ( inaccurate BUT fastest )
+		// linkedlist = BEST EDITING. ( bad reading )
 		
-		Collections.synchronizedList(nameColl);
+		//Vector<String> names=new Vector<String> ();
+		//ArrayList<String> names=new ArrayList<String> ();
+		LinkedList<String> names=new LinkedList<String> ();
 		
-		for(String s:nameColl)
-			System.out.println(s);
-		
-		
-		
-		nameColl.remove("jane");
-		System.out.println(nameColl);
-		System.out.println( nameColl.indexOf("andy"));
-		
-		
-		// HashSet : unique, unordred
-		// LinkedHashSet : unqiue, ordered
-		// TreeSet : unqiue, sorted
-		System.out.println("***** set *****");
-		HashSet<String>  nameSet=new HashSet<String>();
-		nameSet.add("john");
-		nameSet.add("jane");
-		nameSet.add("mike");
-		nameSet.add("john");
-		System.out.println( nameSet );
-		
-		// wrapper class
-		// collections operate only on object/complex data type... not on primitive 
-		// int = Integer , float = Float , double=Double, boolean = Boolean, char=Character
-		// short=Short, long=Long, byte=Byte
-		ArrayList<Integer> scores=new ArrayList<Integer> ();
-		scores.add(10);
-		scores.add(20);
-		scores.add(30);
-		scores.add(22);
-		
+		names.add("john");// 0-john 
+		names.add("jane");// 0-john,1-jane
+		names.add("mike");// 0-john,1-jane,2-mike
+		System.out.println(names);
+		System.out.println(  names.get(1) );// jane
+		names.remove("jane");// 0-john, 1-mike
+		System.out.println(names);// 0-john, 1-mike
+		System.out.println(  names.get(1) );// mike
+		names.add(0,"andy");// 0-andy, 1-john, 2-mike
+		System.out.println(names);// 0-andy, 1-john, 2-mike
+		System.out.println(  names.get(1) );// john
+		names.remove(0);// 0-john, 1-mike
+		System.out.println(names);// 0-john, 1-mike
+		System.out.println(  names.get(1) );// mike
+		names.set(0,"andy");// 0-andy, 1-mike
+		// add( index, value) // insertion, existing value
+							// will move 1 index level down
+		// set( index, value) // update
+							// overwrite existing value
+							// in the index
+		System.out.println(names);// 0-andy, 1-mike
+	
 	}
 
 }
